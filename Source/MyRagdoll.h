72dcc59b8096dbcd1e421173b923380e1239281c
Joseph <jsmgamedev@gmail.com>
2021-10-16 23:07:32 +0200

    Move ragdoll params to separate file

--- Source/MyRagdoll.h
+++ Source/MyRagdoll.h
@@ -4,26 +4,10 @@
 #include "stdafx.h"
 #include "@@headers.h"
 
-enum class JOINT_ENUM
-{
-    JOINT_NO = -1,
-    JOINT_BODY_SPHERICAL = 0,
-    JOINT_BODY_HINGE = 1,
-};
+#include "MyRagdollParams.h"
 
 class MyRagdoll
 {
-    struct JointData
-    {
-        Vec anchor = Vec(0);
-        Vec axis = Vec(0);
-        JOINT_ENUM type = JOINT_ENUM::JOINT_NO;
-        Flt swing = 0.0f;
-        Flt twist = 0.0f;
-        Flt minAngle = 0.0f;
-        Flt maxAngle = 0.0f;
-        Int idx = -1;
-    };
     struct Bone // ragdoll bone
     {
         Char8 name[32]; // name
---
+++ Source/MyRagdollParams.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "stdafx.h"
+#include "@@headers.h"
+
+enum class JOINT_ENUM
+{
+    JOINT_NO = -1,
+    JOINT_BODY_SPHERICAL = 0,
+    JOINT_BODY_HINGE = 1,
+};
+
+struct JointData
+{
+    Vec anchor = Vec(0);
+    Vec axis = Vec(0);
+    JOINT_ENUM type = JOINT_ENUM::JOINT_NO;
+    Flt swing = 0.0f;
+    Flt twist = 0.0f;
+    Flt minAngle = 0.0f;
+    Flt maxAngle = 0.0f;
+    Int idx = -1;
+};
+
+struct RagdollActorData
+{
+    Char8 name[32]; // name
+    JointData jointData;
+};
+
+class RagdollData
+{
+public:
+protected:
+private:
+    Mems<RagdollActorData > _ragdollBones;
+    Flt _density;
+};
